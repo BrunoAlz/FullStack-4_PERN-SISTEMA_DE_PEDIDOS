575853c15a52368fe4e9643919d82960d3fa5af2
Bruno Alves <bruno6821@gmail.com>
2023-05-31 12:08:36 -0300

    Ajustes no servidor, testando endpoint

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -2,8 +2,14 @@ import { Router, Request, Response } from "express";
 
 const router = Router();
 
-router.get("/teste", (req: Request, res: Response) => {
-  return res.json({ ok: true });
+router.get("/test", (req: Request, res: Response) => {
+  return res.json({
+    response: { 
+      route: "http://localhost:8080/test", 
+      method: "GET", 
+      status: 200,
+      message: "Server Working" },
+  });
 });
 
 export { router };
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -3,8 +3,6 @@ import express, { Request, Response, NextFunction } from "express";
 import { router } from "./routes";
 
 const app = express();
-
-app.use(express.json);
 app.use(router);
 
-app.listen(3333, () => console.log("Server Online"));
+app.listen(8080, () => console.log("Server Online"));
