d64c9c7c163f7bd5bcdba67999278af922705c7c
Bruno Alves <bruno6821@gmail.com>
2023-05-31 22:03:55 -0300

    Adiciona as constants para Respostas padronizadas

---
+++ backend/src/constants/responseMessages.ts
@@ -0,0 +1,31 @@
+const responseMessages = {
+  notFound: {
+    error: "Registro não encontrado.",
+  },
+  invalidCredentials: {
+    error: "Credenciais inválidas.",
+  },
+  serverError: {
+    error: "Erro interno do Servidor.",
+  },
+  userNotFound: {
+    error: "Usuário não encontrado.",
+  },
+  incorretPassword: {
+    error: "Senha incorreta.",
+  },
+  emailInUse: {
+    error: "Já existe um usuário com esse e-mail.",
+  },
+  nonexistentToken: {
+    error: "Token de autenticação não fornecido.",
+  },
+  invalidToken: {
+    error: "Token de autenticação inválido.",
+  },
+  userCreated: "Usuário registrado com sucesso.",
+  userUpdated: "Usuário atualizado com sucesso.",
+  userDeleted: "Usuário excluído com sucesso.",
+};
+
+export { responseMessages };
