d2dd990ac4e9bc7d648b3bd543ee0567ca928934
Bruno Alves <bruno6821@gmail.com>
2023-05-31 21:15:24 -0300

    Verifica se o Usuário Existe antes de Criar no Banco

--- backend/src/services/user/CreateUserService.ts
+++ backend/src/services/user/CreateUserService.ts
@@ -1,11 +1,17 @@
 import { CreateUserRequest } from "../../interfaces/UserInterfaces";
 import { UserRepository } from "../../repository/UserRepository";
 
-
 class CreateUserService {
-  async execute({ name, email, password }: CreateUserRequest) {    
+  async execute({ name, email, password }: CreateUserRequest) {
     const userRepository = new UserRepository();
-    return userRepository.createUser({ name, email, password });    
+    const checkEmail = await userRepository.getUserByEmail({ email });
+    
+    if (checkEmail) {
+      throw new Error("Já existe um usuário com este Email.");
+    }
+
+    const user = await userRepository.createUser({ name, email, password });
+    return user;
   }
 }
 
