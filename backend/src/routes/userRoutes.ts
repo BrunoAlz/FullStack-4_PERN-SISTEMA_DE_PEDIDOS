b517b6ce93f1ff5a33f10cbbf0bf1edf3c6d3c16
Bruno Alves <bruno6821@gmail.com>
2023-06-04 17:12:36 -0300

    Ajustando a instancia do Controller nas rotas do usuario

--- backend/src/routes/userRoutes.ts
+++ backend/src/routes/userRoutes.ts
@@ -1,20 +1,13 @@
 import { Router } from "express";
 import { UserController } from "../controllers/user/UserController";
-import {
-  userCreateValidation,
-  userLoginValidation,
-} from "../middlewares/userValidations";
+import { userCreateValidation } from "../middlewares/userValidations";
 import { isAuth, validate } from "../middlewares/handleValidation";
 
 const router = Router();
+const userController = new UserController();
 
-router.post(
-  "/create",
-  userCreateValidation(),
-  validate,
-  new UserController().create
-);
+router.post("/create", userCreateValidation(), validate, userController.create);
 
-router.get("/details", isAuth, new UserController().details);
+router.get("/details", isAuth, userController.details);
 
 export { router as userRoutes };
