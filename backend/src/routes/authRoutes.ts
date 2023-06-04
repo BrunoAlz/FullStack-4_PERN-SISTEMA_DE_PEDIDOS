3df9a04ddfe28e526dc8561fabd402cd559084c2
Bruno Alves <bruno6821@gmail.com>
2023-06-04 17:13:27 -0300

    Ajustando a instancia do Controller nas rotas de Auth

--- backend/src/routes/authRoutes.ts
+++ backend/src/routes/authRoutes.ts
@@ -4,12 +4,8 @@ import { userLoginValidation } from "../middlewares/userValidations";
 import { validate } from "../middlewares/handleValidation";
 
 const router = Router();
+const authController = new AuthUserController();
 
-router.post(
-  "/login",
-  userLoginValidation(),
-  validate,
-  new AuthUserController().login
-);
+router.post("/login", userLoginValidation(), validate, authController.login);
 
 export { router as authRoutes };
