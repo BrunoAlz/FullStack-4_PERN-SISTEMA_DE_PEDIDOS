e5055b0c95c5d367b98f2f20f4ab128623cba3e8
Bruno Alves <bruno6821@gmail.com>
2023-06-13 13:30:13 -0300

    Implementando o Componente de Input e Textarea

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,9 +1,27 @@
+import Head from 'next/head'
+import Image from 'next/image'
 import style from '../styles/Home.module.scss'
+import logoImg from '../../public/logo.svg'
+
+import { Input } from '@/components/ui/input'
+
 
 export default function Home() {
   return (
-    <div className={style.container}>
-      <h1>PEDIDOS</h1>
-    </div>
+    <>
+      <Head>
+        <title>Dog-Pedidos | Fazer Login</title>
+      </Head>
+      <div className={style.containerCenter}>
+        <Image src={logoImg} alt={'LogoDogzera'} />
+
+        <div className={style.login}>
+          <form>
+            <Input placeholder='Digite seu Email' type='text' />
+            <Input placeholder='Digite sua Senha' type='password' />
+          </form>
+        </div>
+      </div>
+    </>
   )
 }
