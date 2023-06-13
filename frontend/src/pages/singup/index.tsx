c1c35575b132c0f3e1822e04de1bee33b155936f
Bruno Alves <bruno6821@gmail.com>
2023-06-13 18:39:42 -0300

    Ajustando o Form de Cadastro de Usuários

--- frontend/src/pages/singup/index.tsx
+++ frontend/src/pages/singup/index.tsx
@@ -1,42 +1,71 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import style from '../../styles/Home.module.scss'
-import logoImg from '../../../public/logo.svg'
+import { FormEvent, useState } from "react";
+import Head from "next/head";
+import Image from "next/image";
+import style from "../../styles/Home.module.scss";
+import logoImg from "../../../public/logo.svg";
 
-import { Input } from '@/components/ui/input'
-import { Button } from '@/components/ui/button'
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
 
-import Link from 'next/link'
+import Link from "next/link";
+import { toast } from "react-toastify";
 
 export default function Singup() {
-    return (
-        <>
-            <Head>
-                <title>Dog-Pedidos | Cadastrar</title>
-            </Head>
-            <div className={style.containerCenter}>
-                <Image src={logoImg} alt={'LogoDogzera'} />
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [loading, setLoading] = useState(false);
 
-                <div className={style.login}>
+  async function handleSingUp(event: FormEvent) {
+    event.preventDefault();
 
-                    <h1>Criar conta</h1>
+    if (name === '' || email === '' || password === '') {
+      toast.error("Preencha todos os campos");
+      return;
+    }
 
-                    <form>
-                        <Input placeholder='Digite seu Nome' type='text' />
-                        <Input placeholder='Digite seu Email' type='text' />
-                        <Input placeholder='Digite sua Senha' type='password' />
-                        <Button type="button" loading={false}>
-                            Cadastrar
-                        </Button>
-                    </form>
+    setLoading(true)
+  }
 
-                    <Link href="/">
-                        <p className={style.text}>
-                            Já possui uma conta? faça login!
-                        </p>
-                    </Link>
-                </div>
-            </div>
-        </>
-    )
+  return (
+    <>
+      <Head>
+        <title>Dog-Pedidos | Cadastrar</title>
+      </Head>
+      <div className={style.containerCenter}>
+        <Image src={logoImg} alt={"LogoDogzera"} />
+
+        <div className={style.login}>
+          <h1>Criar conta</h1>
+
+          <form onSubmit={handleSingUp}>
+            <Input
+              placeholder="Digite seu Nome"
+              type="text"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            />
+            <Input
+              placeholder="Digite seu Email"
+              type="text"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)} />
+
+            <Input
+              placeholder="Digite sua Senha"
+              type="password" value={password}
+              onChange={(e) => setPassword(e.target.value)} />
+
+            <Button type="submit" loading={loading}>
+              Cadastrar
+            </Button>
+          </form>
+
+          <Link href="/">
+            <p className={style.text}>Já possui uma conta? faça login!</p>
+          </Link>
+        </div>
+      </div>
+    </>
+  );
 }
